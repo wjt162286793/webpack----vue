5e7bfc04d870549522191263f07fe7cd0bacd750
wjt162286793 <78353075+wjt162286793@users.noreply.github.com>
2023-04-13 14:00:39 +0800

    添加webpack.config的注释

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+//引入需要的依赖包
 const path = require('path')
 const toml = require('toml');
 const yaml = require('yamljs');
@@ -7,40 +8,54 @@ const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = (env) => {
     return {
+        //模式:development是开发模式,production是生产模式
         mode: 'development',
+        //入口文件,也可以配置多入口,具体看文档
         entry: './src/main.js',
+        //输出配置
         output: {
+            //文件名,使用hash算法生成文件名
             filename: '[name].[contenthash].js',
+            //构建后文件的存放目录
             path: path.resolve(__dirname, 'dist'),
             publicPath: '/',
         },
+        //代码源文件映射,方便调试
         devtool: 'inline-source-map',
+        //配置各种loader
         module: {
             rules: [
+                //vue的loader
                 {
                     test: /\.vue$/,
                     loader: 'vue-loader'
                 },
+                //css的编译loader,这里记得把vue-style-loader写到前面
                 {
                     test: /\.css$/i,
                     use: ['vue-style-loader', 'style-loader', 'css-loader'],
                 },
+                //各类图片的loader
                 {
                     test: /\.(png|svg|jpg|jpeg|gif)$/i,
                     type: 'asset/resource',
                 },
+                //字体文件的loader
                 {
                     test: /\.(woff|woff2|eot|ttf|otf)$/i,
                     type: 'asset/resource',
                 },
+                //csv等表格的解析loader
                 {
                     test: /\.(csv|tsv)$/i,
                     use: ['csv-loader'],
                 },
+                //xml标记文本的loader
                 {
                     test: /\.xml$/i,
                     use: ['xml-loader'],
                 },
+                //toml类型文件的loader,其实吧,这个文件是干啥的,我也不知道
                 {
                     test: /\.toml$/i,
                     type: 'json',
@@ -48,6 +63,7 @@ module.exports = (env) => {
                         parse: toml.parse,
                     },
                 },
+                //yaml文件的loader
                 {
                     test: /\.yaml$/i,
                     type: 'json',
@@ -55,6 +71,7 @@ module.exports = (env) => {
                         parse: yaml.parse,
                     },
                 },
+                //json5文件的loader
                 {
                     test: /\.json5$/i,
                     type: 'json',
@@ -62,6 +79,7 @@ module.exports = (env) => {
                         parse: json5.parse,
                     },
                 },
+                //html的配置loader
                 {
                     test: /\.html$/i,
                     loader: "html-loader",
@@ -69,6 +87,7 @@ module.exports = (env) => {
                         sources: false,
                     },
                 },
+                //babel的配置loader,将es6+的代码转化为es5的
                 {
                     test: /\.m?js$/,
                     exclude: /(node_modules|bower_components)/,
@@ -80,11 +99,13 @@ module.exports = (env) => {
                         }
                     }
                 },
+                //js的源代码映射
                 {
                     test: /\.js$/,
                     enforce: "pre",
                     use: ["source-map-loader"],
                 },
+                //less的解析loader
                 {
                     test: /\.less$/i,
                     use: [
@@ -97,7 +118,9 @@ module.exports = (env) => {
 
             ],
         },
+        //插件
         plugins: [
+            //这里是html的配置,目标指向public下面的index.html,也可以在这里配置里面一些东西
             new HtmlWebpackPlugin({
                 // title: 'webpack搭建vue脚手架',
                 minify: {
@@ -110,11 +133,14 @@ module.exports = (env) => {
 
                 template: './public/index.html'
             }),
+            //vue的插件
             new VueLoaderPlugin()
         ],
+        //开发服务器
         devServer: {
             static: './dist',
         },
+        //依赖包的抽取,减少大量的冗余代码,可以去看看
         optimization: {
             moduleIds: 'deterministic',
             runtimeChunk: 'single',
@@ -128,6 +154,7 @@ module.exports = (env) => {
                 },
             },
         },
+        //配置路径的快捷引用
               resolve:{
           extensions:['.js','.vue','json','.css','.less'],
           alias:{
