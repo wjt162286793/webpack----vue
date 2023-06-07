a8353f7120658771fd126a8e512762169475061b
wangjingtao <hju2@qq.com>
2023-06-07 18:02:26 +0800

    公共组件封装

--- src/components/globalList.vue
+++ src/components/globalList.vue
@@ -1,121 +1,234 @@
 <template>
     <div class="listContent">
-      <div class="searchbox">
-        <template v-for="(item,index) in templateData.searchTemplate" :key="index">
-            <div v-if="item.type === 'input'">
-                <span>{{ item.label }}:</span>
-                <el-input v-model="templateData.searchData[item.name]" class="searchItem"></el-input>
-            </div>
-            <div v-else-if="item.type === 'select'" >
-                <span>{{ item.label }}:</span>
-                <el-select class="searchItem" v-model="templateData.searchData[item.name]">
-                    <el-option v-for="(val,ind) in item.options" :key="ind" :label="val.label" :value="val.value"></el-option>
-                </el-select>
-            </div>
-            <div v-else-if="item.type === 'button'" class="searchBtn">
-               <el-button :type="item.btnType" @click="btnClick(item.event)">{{ item.text }}</el-button>
-            </div>
+        <div class="searchbox">
+            <template v-for="(item, index) in templateData.searchTemplate" :key="index">
+                <div v-if="item.type === 'input'">
+                    <!-- <span>{{ item.label }}:</span> -->
+                    <el-input v-model="templateData.searchData[item.name]" class="searchItem"
+                        :placeholder="item.placeholder"></el-input>
+                </div>
+                <div v-else-if="item.type === 'select'">
+                    <!-- <span>{{ item.label }}:</span> -->
+                    <el-select class="searchItem" v-model="templateData.searchData[item.name]"
+                        :placeholder="item.placeholder">
+                        <el-option v-for="(val, ind) in item.options" :key="ind" :label="val.label"
+                            :value="val.value"></el-option>
+                    </el-select>
+                </div>
+                <div v-else-if="item.type === 'button'" class="searchBtn">
+                    <el-button :type="item.btnType" @click="btnClick(item.event)">{{ item.text }}</el-button>
+                </div>
+            </template>
+        </div>
+        <div class="tableBox">
+            <el-table :border="true" ref="multipleTableRef" :data="tableData" style="width: 100%"
+                @selection-change="handleSelectionChange">
+                <!-- <el-table-column type="selection" width="55" />
+                <el-table-column label="Date" width="120">
+                    <template #default="scope">{{ scope.row.date }}</template>
+                </el-table-column>
+                <el-table-column property="name" label="Name" width="120" />
+                <el-table-column property="address" label="Address" show-overflow-tooltip /> -->
+                <template v-for="(item,index) in tableHeader" :key="index">
+                    <el-table-column v-if="!item.scoped"  :property="item.property" :width="item.width" :label="item.label"></el-table-column>
+                    <el-table-column v-else :width="item.width" :label="item.label">
+                        <template #default="scope">{{ scope.row[item.property] }}</template>
+                    </el-table-column>
+                </template>
+                 
+            </el-table>
+        </div>
+        <div class="pagination">
+      <el-pagination
+        small
+        background
+        layout="prev, pager, next, jumper, ->, total"
+        :total="total"
+        class="mt-4"
+        v-model:page-size="pageSize"
+        v-model:current-page="currentPage"
+        @size-change="reqList"
+        @current-change="reqList"
+        @prev-click="reqList"
+        @next-click="reqList"
+      >
+        <template #pager-text>
+          <span>共 {{ total }} 条数据</span>
         </template>
-      </div>
+      </el-pagination>
+    </div>
     </div>
 </template>
 <script setup>
-
 const props = defineProps({
-    modeType:String
+    modeType: String
 })
-let  templateData = reactive({
-        searchTemplate:[
-            {
-                name:'entiryName',
-                type:'input',
-                label:'实体名称',
-                placeholder:'请输入实体名称',
-                show:true,
-            },
-            {
-                name:'entiryType',
-                type:'select',
-                label:'实体类型',
-                placeholder:'请选择实体类型',
-                options:[
-                    {
-                        label:'实物资产',
-                        value:1
-                    },
-                    {
-                        label:'机械设备',
-                        value:2
-                    },
-                    {
-                        label:'个体资产',
-                        value:3
-                    },
-                    {
-                        label:'IT资产',
-                        value:4
-                    }
-                ],
-                show:true,
-            },
-            {
-                name:'searchBtn',
-                type:'button',
-                btnType:'primary',
-                text:'查询',
-                event:'search'
-            },
-            {
-                name:'addBtn',
-                type:'button',
-                btnType:'primary',
-                text:'新增',
-                event:'toAdd'
-            }
-        ],
-        tableTemplate:[],
-        pagination:{},
-        searchData:{
-            entiryName:'222222222',
-            entiryType:''
+let multipleTableRef = ref(null)
+let templateData = reactive({
+    searchTemplate: [
+        {
+            name: 'entiryName',
+            type: 'input',
+            label: '实体名称',
+            placeholder: '请输入实体名称',
+            show: true,
+        },
+        {
+            name: 'entiryType',
+            type: 'select',
+            label: '实体类型',
+            placeholder: '请选择实体类型',
+            options: [
+                {
+                    label: '实物资产',
+                    value: 1
+                },
+                {
+                    label: '机械设备',
+                    value: 2
+                },
+                {
+                    label: '个体资产',
+                    value: 3
+                },
+                {
+                    label: 'IT资产',
+                    value: 4
+                }
+            ],
+            show: true,
+        },
+        {
+            name: 'searchBtn',
+            type: 'button',
+            btnType: 'primary',
+            text: '查询',
+            event: 'search'
+        },
+        {
+            name: 'addBtn',
+            type: 'button',
+            btnType: 'primary',
+            text: '新增',
+            event: 'toAdd'
         }
-    })
-const searchList = ()=>{
-    console.log(templateData.searchData,'搜索项')
+    ],
+    tableTemplate: [],
+    pagination: {},
+    searchData: {
+        entiryName: '',
+        entiryType: ''
+    }
+})
+let tableHeader = ref([
+    {
+        property:'date',
+        label:'日期',
+        width:200,
+        scope:false
+    },
+    {
+        property:'name',
+        label:'姓名',
+        width:200,
+        scope:false
+    },
+    {
+        property:'address',
+        label:'地址',
+        width:'width',
+        scope:false
+    },
+    {
+        property:'action',
+        label:'操作',
+        width:200,
+        scope:true,
+        scopeTemplate:[
+            {
+                source:'el-icon',
+                type:'primary',
+                icon:'Edit',
+                boxtype:'circle',
+                style:null,
+                text:'修改'
+            },
+            {
+                source:'el-icon',
+                type:'danger',
+                icon:'Delete',
+                boxtype:'circle',
+                style:null,
+                text:'删除'
+            },
+        ]
+    },
+])
+const tableData = ref([])
+const total = ref(0);
+let pageSize = 10
+let currentPage = ref(1)
+const handleSelectionChange = (val) => {
+    //   multipleSelection.value = val
 }
-const toAdd = () =>{
+const reqList = () => {
+    // console.log(templateData.searchData,'搜索项')
+    tableData.value = [{
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },]
+}
+const toAdd = () => {
     console.log('新增')
 }
-const btnClick = (btnFlag)=>{
-    switch(btnFlag){
+const btnClick = (btnFlag) => {
+    switch (btnFlag) {
         case 'search':
-        searchList()
-        break
+            reqList()
+            break
         case 'toAdd':
-        toAdd()
+            toAdd()
     }
 }
-onMounted(()=>{
-    console.log(props.modeType,'多少??')
-    console.log(templateData.searchData.entiryName,'????')
+onMounted(() => {
+    console.log(props.modeType, '多少??')
+    console.log(templateData.searchData.entiryName, '????')
+    console.log(multipleTableRef, '表格')
 
 })
 </script>
 <style lang="less">
-.listContent{
+.listContent {
     height: 100%;
     position: relative;
     margin-bottom: 50px;
     padding: 10px;
-    .searchbox{
+
+    .searchbox {
         display: flex;
-        .searchItem{
+
+        .searchItem {
             width: 240px;
-            margin:0 20px 0 10px;
+            margin: 0 20px 0 0px;
         }
-        .searchBtn{
+
+        .searchBtn {
             margin-right: 10px;
+            margin-bottom: 20px;
         }
     }
+    .pagination {
+  height: 30px;
+  position: absolute;
+  bottom: 20px;
+  right: 0px;
+  margin-right: 30px;
+}
 }
 </style>
